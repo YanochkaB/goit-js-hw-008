1a1639d22321ccf0f6596e17102edd8319a6b894
YanochkaB <yanabukhtij@gmail.com>
2022-08-29 10:23:57 +0300

    шукаю помилку

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,53 @@
 import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
 const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
 };
-// console.log(refs)
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+refs.textarea.addEventListener('input', throttle(onTextareaInput), 500);
+
+refs.form.addEventListener('input', evt => {
+    formData[evt.currentTarget.name] = evt.currentTarget.value;
+    console.log(formData) //об'єкт email and message
+})
 
 populateTextarea();
 
-//робота з текстовим полем 
+//робота з текстовими полями (очищення після відправки форми) +++
 function onFormSubmit(evt) {
     evt.preventDefault();
-    evt.currentTarget.reset();
+    evt.currentTarget.reset(); //очистити поля форми
+    localStorage.removeItem(STORAGE_KEY); //очистити локальні дані
 }
 
-//отримання даних з полів
+//отримання даних з полів і запис у локал сторідж (одним рядком) +++
 function onTextareaInput(evt) {
-    const message = evt.currentTarget.value;
-    localStorage.setItem('feedback-form-state', message);
+    const message = JSON.stringify(formData); //значення текстових полів (рядок)
+
+    localStorage.setItem(STORAGE_KEY, message); //додаєм текст з поля в локал сторідж
 }
 
 //занесення даних при перезавантаженні сторінки
 function populateTextarea() {
-    const savedMessage = localStorage.getItem('feedback-form-state');
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
 
     savedMessage ? refs.textarea.value = savedMessage : '';
     
+    // if (savedMessage) {
+    //     // refs.textarea = refs.textarea.value;
+    // } else {
+    //     ''
+    // }
 }
+
+
+// const json = '{"name":"Mango","age":3,"isHappy":true}';
+
+// const dog = JSON.parse(json);
+// console.log(dog); // {name: "Mango", age: 3, isHappy: true}
+// console.log(dog.name); // "Mango"
